f6a0213df6629744ef3d8fb32b806b60050a53bb
kirmann <k.dkle@hotmail.fr>
2022-03-13 10:47:39 +0100

    home page ok

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ let vid = document.querySelector(".video__container")
 vid.playbackRate = 0.6
 const title = document.querySelector(".title")
 let viewPortWidth = screen.width
-// viewPortWidth > 840
-//     ? (title.innerHTML = "Garage Mobile Santos")
-//     : (title.innerHTML = "mécanique à domicile")
-//changement du titre en mode plein écran
-const getViewPortWidth = () => {
-    viewPortWidth = screen.width
-    viewPortWidth < 840
-        ? (title.innerHTML = "Garage <br/> mobile <br/>santos")
-        : (title.innerHTML = "Mécanique<br/> à<br/> domicile")
-}
-getViewPortWidth()
-window.addEventListener("resize", getViewPortWidth)
+
+// const getViewPortWidth = () => {
+//     viewPortWidth = screen.width
+//     viewPortWidth < 840
+//         ? (title.innerHTML = "Garage <br/> mobile <br/>santos")
+//         : (title.innerHTML = "Mécanique<br/> à<br/> domicile")
+// }
+// getViewPortWidth()
+// window.addEventListener("resize", getViewPortWidth)
