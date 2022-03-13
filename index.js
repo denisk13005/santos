fa13139c0e4e1d74a22150ae320a89f10c401fb4
kirmann <k.dkle@hotmail.fr>
2022-03-13 10:49:04 +0100

    modif js

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 let vid = document.querySelector(".video__container")
-vid.playbackRate = 0.6
-const title = document.querySelector(".title")
-let viewPortWidth = screen.width
-
-// const getViewPortWidth = () => {
-//     viewPortWidth = screen.width
-//     viewPortWidth < 840
-//         ? (title.innerHTML = "Garage <br/> mobile <br/>santos")
-//         : (title.innerHTML = "Mécanique<br/> à<br/> domicile")
-// }
-// getViewPortWidth()
-// window.addEventListener("resize", getViewPortWidth)
+vid.playbackRate = 0.8
